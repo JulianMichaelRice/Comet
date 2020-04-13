497e06d68f4f1dda48a30ea59371797aa40c7e48
Julian M. Rice <rice.m.julian@gmail.com>
2020-04-13 04:10:24 -0700

    Working to some degree. Animations are still a bit funky. Looks great

---
+++ faded.js
@@ -0,0 +1,14 @@
+$(document).on("scroll", function () { //Using the scroll global variable
+    pageTop = $(document).scrollTop();
+    var fades =  $(".bgn"); //Calling the class in HTML
+    var windowBottom  = pageTop + $(window).height();
+    for (var i = 0; i < fades.length; i++) {
+        var now = fades[i];
+        if ($(now).position().top < windowBottom) {
+            $(now).addClass("fadefade");
+        }
+        else {
+            $(now).removeClass("hidden");
+        }
+    }
+});
